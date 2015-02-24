91328d835e9513565713d7d26921d592aaa54b31
appvise <cchong.vise@gmail.com>
2015-02-23 20:18:19 -0800

    Update ash.sh

--- ash.sh
+++ ash.sh
@@ -1,7 +1,7 @@
 #debian
 touch /root/blank.txt
-cp -r blank.txt > /etc/apt/sources.list 
-cp -r blank.txt > ~/.bash_profile
+cp -r /root/blank.txt > /etc/apt/sources.list 
+cp -r /root/blank.txt > ~/.bash_profile
 mkdir /etc/iptables
 mkdir /var/iptables
 
@@ -34,10 +34,10 @@ echo "iptables-restore < /etc/iptables/rulez" >> ~/.bash_profile
 echo "iptables-restore < /etc/iptables/rulez" >> ~/.bash_profile
 echo "deb http://ftp.us.debian.org/debian/ wheezy main" >> /etc/apt/sources.list
 echo "deb-src http://ftp.us.debian.org/debian/ wheezy main" >> /etc/apt/sources.list
-echo "deb http://security.debian.org/debian/ wheezy/updates main" >> /etc/apt/sources.list
-echo "deb-src http://security.debian.org/debian/ wheezy/updates main" >> /etc/apt/sources.list
-echo "deb http://ftp.us.debian.org/debian/ wheezy/updates main" >> /etc/apt/sources.list
-echo "deb-src http://ftp.us.debian.org/debian/ wheezy/updates main" >> /etc/apt/sources.list
+echo "deb http://security.debian.org/ wheezy/updates main" >> /etc/apt/sources.list
+echo "deb-src http://security.debian.org/ wheezy/updates main" >> /etc/apt/sources.list
+echo "deb http://ftp.us.debian.org/debian/ wheezy-updates main" >> /etc/apt/sources.list
+echo "deb-src http://ftp.us.debian.org/debian/ wheezy-updates main" >> /etc/apt/sources.list
 echo "deb http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
 echo "deb-src http://packages.dotdeb.org wheezy all">> /etc/apt/sources.list
 apt-get update -y

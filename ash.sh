6219001e365ced9b242cf500aa898a6cbe02e2f6
appvise <cchong.vise@gmail.com>
2015-02-23 21:13:08 -0800

    Update ash.sh

--- ash.sh
+++ ash.sh
@@ -1,33 +1,46 @@
 #debian
-touch /root/blank.txt
-cp -r /root/blank.txt > /etc/apt/sources.list 
-cp -r /root/blank.txt > ~/.bash_profile
-mkdir /etc/iptables
-mkdir /var/iptables
+\unalias -a
 
-iptables --flush
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -t nat -F
-iptables -t mangle -F
-iptables -F
-iptables -X
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+\/etc/init.d/cron stop
+\update-rc.d cron disable
+\/etc/init.d/netcat stop
+\update-rc.d/netcat disable
+\/etc/init.d/ssh stop
+\update-rc.d/ssh disable
+\/etc/init.d/telnet stop
+\update-rc.d/telnet disable
+\export $PATH=/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/local/sbin
+\chattr -i /etc/passwd
+\rm -r /root/blank*
+\touch /root/blank.txt
+\cp -r /root/blank.txt /etc/apt/sources.list 
+\cp -r /root/blank.txt ~/.bash_profile
+\mkdir /etc/iptables
+\mkdir /var/iptables
+
+\iptables --flush
+\iptables -P INPUT ACCEPT
+\iptables -P FORWARD ACCEPT
+\iptables -P OUTPUT ACCEPT
+\iptables -t nat -F
+\iptables -t mangle -F
+\iptables -F
+\iptables -X
+\iptables -A INPUT -i lo -j ACCEPT
+\iptables -A INPUT ! -i lo -d 127.0.0.0/8 -j REJECT
+\iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+\iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+\iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 #iptables -A INPUT -p udp --dport 53 -j ACCEPT
 #iptables -A INPUT -p tcp -s $teamIP1 --sport 1024:65535 -d $IPaddress --dport $hostport1 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
-iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j REJECT
-iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
-iptables -A INPUT -j REJECT
-iptables -A FORWARD -j REJECT
-iptables -A OUTPUT -j ACCEPT
-iptables -S > /etc/iptables/rulez
-iptables -S > /var/iptables/rulez
+\iptables -A INPUT -p tcp -m state --state NEW --dport 22 -j ACCEPT
+\iptables -A INPUT -p icmp -m icmp --icmp-type 8 -j REJECT
+\iptables -A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
+\iptables -A INPUT -j REJECT
+\iptables -A FORWARD -j REJECT
+\iptables -A OUTPUT -j ACCEPT
+\iptables -S > /etc/iptables/rulez
+\iptables -S > /var/iptables/rulez
 sed -i '1s/.*/*filter/' /etc/iptables/rulez
 echo "COMMIT" >> /etc/iptables/rulez
 echo "iptables-restore < /etc/iptables/rulez" >> ~/.bash_profile
@@ -66,10 +79,10 @@ python /tmp/artillery/setup.py
 #nano +33 /var/artillery/config
 cd /var/artillery
 #sed -i '18s/.*/MONITOR_FOLDERS="/proc","/sys","/sh","/tmp","/home","/dev","/lib","/lib64","/opt","/run","/srv","/var/www","/etc","/var","/bin","/sbin","/usr","/boot"' /var/artillery/config
-sed -i '30s/.*/HONEYPOT_BAN="ON"' /var/artillery/config
-#sed -i '69s/.*/SSH_BRUTE_ATTEMPTS="1"' /var/artillery/config
-#sed -i '72s/.*/FTP_BRUTE_MONITOR="ON"' /var/artillery/config
-#sed -i '75s/.*/FTP_BRUTE_ATTEMPTS="1"' /var/artillery/config
+sed -i '30s/.*/HONEYPOT_BAN="ON"/' /var/artillery/config
+sed -i '69s/.*/SSH_BRUTE_ATTEMPTS="1"/' /var/artillery/config
+sed -i '72s/.*/FTP_BRUTE_MONITOR="ON"/' /var/artillery/config
+sed -i '75s/.*/FTP_BRUTE_ATTEMPTS="1"/' /var/artillery/config
 #echo "Edit Bind Interface, Example: 192.168.0.22; press Enter to Edit line in Config file"
 #read $pressEnter
 #nano +97 /var/artillery/config

9799390de6e54eeb9fb900a8ce688491ca6092dd
appvise <cchong.vise@gmail.com>
2015-02-27 20:18:24 -0800

    Update artillery.sh

--- artillery.sh
+++ artillery.sh
@@ -1,12 +1,9 @@
 # Debian Artillery Script by c2_
 \apt-get install git
-\cd /tmp
-\rm -r /tmp/artillery
+\rm -r /var/artillery
 \git clone https://github.com/trustedsec/artillery
-\cp -r ash/artillery/setup.py /tmp/artillery
-\/bin/busybox rm -r /var/artillery
-python /tmp/artillery/setup.py
-\cp -r ash/artillery/config /var/artillery
-python /tmp/artillery/restart_server.py
+\cp -r ash/artillery/setup.py /root/artillery
+\python setup.py
+\python restart_server.py
 
 

9684e3953fffb09e9ade7c1275d1e99764ebed95
appvise <cchong.vise@gmail.com>
2015-02-27 20:28:18 -0800

    Update setup.py

--- artillery/setup.py
+++ artillery/setup.py
@@ -17,8 +17,9 @@ Written by: Dave Kennedy (ReL1K)
 
 
 
-if not os.path.isfile("/etc/init.d/artillery"):
-    answer = "yes"
+
+
+answer = "yes"    
 if answer.lower() == "y" or answer.lower() == "yes":
     if is_posix():
         kill_artillery()

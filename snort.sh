02a7787d7e4c5b6fddbc67ca4b144003cc4e7d4e
appvise <cchong.vise@gmail.com>
2015-02-27 23:00:20 -0800

    Update snort.sh

--- snort.sh
+++ snort.sh
@@ -1,5 +1,5 @@
 \rm -r /usr/src/*
-\apt-get install wget nano make g++ bison flex git python sed htop libpcre3-dev zlib1g-dev libpcap-dev -y
+\apt-get install wget nano make g++ bison flex git python sed htop libpcre3-dev libdumbnet-dev zlib1g-dev libpcap-dev -y
 cd /usr/src
 wget http://www.tcpdump.org/release/libpcap-1.6.2.tar.gz  http://libdnet.googlecode.com/files/libdnet-1.12.tgz https://www.snort.org/downloads/snort/daq-2.0.4.tar.gz https://snort.org/downloads/snort/snort-2.9.7.0.tar.gz
 tar -xzf libpcap-1.6.2.tar.gz && cd /usr/src/libpcap-1.6.2

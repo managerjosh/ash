7fa345d3ce4f8c692058edce68b8b51781ad6228
appvise <cchong.vise@gmail.com>
2015-03-01 21:53:38 -0800

    Create install-gentoo.sh

---
+++ install-gentoo.sh
@@ -0,0 +1,10 @@
+# mount /dev/sda3 /mnt/gentoo
+# mkdir /mnt/gentoo/boot
+# swapon /dev/sda2
+# mount /dev/sda1 /mnt/gentoo
+# cd /mnt/gentoo
+# link
+
+wget ftp://ftp.ucsb.edu/pub/mirrors/linux/gentoo/snapshots/portage-latest.tar.bz2
+wget ftp://ftp.ucsb.edu/pub/mirrors/linux/gentoo/releases/amd64/autobuilds/current-stage3-amd64
+tar -xzf current-stage3-amd64

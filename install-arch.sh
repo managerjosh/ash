6a85faf0aa643ddf9cc877a57e49c7893cc84839
appvise <cchong.vise@gmail.com>
2015-03-01 22:26:08 -0800

    Update install-arch.sh

--- install-arch.sh
+++ install-arch.sh
@@ -7,17 +7,20 @@
 # mkdir /mnt/boot
 # mount /dev/sda1 /mnt/boot
 # cd /mnt
-pacstrap /mnt base
-genfstab -p /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
+# pacstrap /mnt base
+# genfstab -p /mnt >> /mnt/etc/fstab
+# cp -r /etc/resolv.conf /mnt/etc/resolv.conf
+# arch-chroot /mnt
+# pacman -S git
 echo archlinux > /etc/hostname
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 locale-gen
 mkinitcpio -p linux
 echo "Type in your password"
 passwd
-pacman -S grub
+pacman -S grub -y
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 cp -r /etc/resolv.conf /mnt/etc/resolv.conf
+exit
 reboot

7165783d8057bbdddd24edf06d2c507cf1a65a9e
appvise <cchong.vise@gmail.com>
2015-03-01 23:02:30 -0800

    Update vpn-centos.sh

--- vpn-centos.sh
+++ vpn-centos.sh
@@ -1,4 +1,4 @@
-yum install wget
+yum install wget -y
 wget http://swupdate.openvpn.org/as/openvpn-as-2.0.12-CentOS7.x86_64.rpm
 rpm -i openvpn-as-2.0.12-CentOS7.x86_64.rpm
 echo "Set password for user: openvpn"

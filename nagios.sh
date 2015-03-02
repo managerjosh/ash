4787b9f81bb099c88235e437b29c426a3abcbc4e
appvise <cchong.vise@gmail.com>
2015-03-01 23:09:25 -0800

    Update nagios.sh

--- nagios.sh
+++ nagios.sh
@@ -1,4 +1,3 @@
 # monitor:~# apt-get install nagios3 nagios-plugins nagios-nrpe-plugin
 # server1:~# apt-get install nagios-nrpe-server nagios-plugins
 \apt-get install nagios-nrpe-server nagios3 nagios-plugins nagios-nrpe-plugin
-

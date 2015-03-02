35bb7ac8697d079fa8d22d9781a6df72de68e2dc
appvise <cchong.vise@gmail.com>
2015-03-02 00:36:12 -0800

    Update artillery.sh

--- artillery.sh
+++ artillery.sh
@@ -1,5 +1,6 @@
-# Debian Artillery Script by c2_
+# Artillery Script by c2_
 \rm -r /var/artillery
+\rm -r -f /var/artillery
 \git clone https://github.com/trustedsec/artillery
 \cp -r ash/artillery/setup.py /root/artillery
 \python /root/artillery/setup.py

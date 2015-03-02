7f60aeaad958dfdd3c90c25b70287c9c993b7ce7
appvise <cchong.vise@gmail.com>
2015-03-02 00:36:54 -0800

    Update artillery.sh

--- artillery.sh
+++ artillery.sh
@@ -3,6 +3,7 @@
 \rm -r -f /var/artillery
 \git clone https://github.com/trustedsec/artillery
 \cp -r ash/artillery/setup.py /root/artillery
+\cp -r ash/artillery/config /root/artillery
 \python /root/artillery/setup.py
 \python /root/artillery/restart_server.py
 

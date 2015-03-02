46c1b6f3d1540839f9674d0cd4207d726f32fe1a
appvise <cchong.vise@gmail.com>
2015-03-01 16:02:46 -0800

    Update secure-ubuntu.sh

--- secure-ubuntu.sh
+++ secure-ubuntu.sh
@@ -51,6 +51,18 @@
 \/bin/busybox echo "deb http://us.archive.ubuntu.com/ubuntu/ trusty-backports main restricted universe multiverse" >> /etc/apt/sources.list
 \/bin/busybox echo "deb-src http://us.archive.ubuntu.com/ubuntu/ trusty-backports main restricted universe multiverse" >> /etc/apt/sources.list
 
+\/bin/busybox echo "deb http://us.archive.ubuntu.com/ubuntu/ trusty-security mainnrestricted" >> /etc/apt/sources.list
+\/bin/busybox echo "deb-src http://us.archive.ubuntu.com/ubuntu/ trusty-security main restricted" >> /etc/apt/sources.list
+
+\/bin/busybox echo "deb http://security.ubuntu.com/ubuntu/ trusty-security main restricted" >> /etc/apt/sources.list
+\/bin/busybox echo "deb-src http://security.ubuntu.com/ubuntu/ trusty-security main restricted" >> /etc/apt/sources.list
+\/bin/busybox echo "deb http://security.ubuntu.com/ubuntu/ trusty-security main universe" >> /etc/apt/sources.list
+\/bin/busybox echo "deb-src http://security.ubuntu.com/ubuntu/ trusty-security main universe" >> /etc/apt/sources.list
+\/bin/busybox echo "deb http://security.ubuntu.com/ubuntu/ trusty-security main multiverse" >> /etc/apt/sources.list
+\/bin/busybox echo "deb-src http://security.ubuntu.com/ubuntu/ trusty-security main multiverse" >> /etc/apt/sources.list
+
+
+
 
 \apt-get update -y
 \apt-get upgrade -y

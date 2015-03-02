ac6cf9178fce40caa8b6be622ab4686d281b8f9a
appvise <cchong.vise@gmail.com>
2015-03-02 00:49:02 -0800

    Update x-debian.sh

--- x-debian.sh
+++ x-debian.sh
@@ -1,8 +1,4 @@
 # execute all scripts
-bash ash/secure.sh
-bash ash/artillery.sh
-bash ash/snort.sh
-bash ash/logstash.sh
-bash ash/kibana.sh
-bash ash/ossec.sh
-bash ash/bro.sh
+bash ash/secure/debian.sh
+bash ash/artillery/artillery.sh
+bash ash/snort/debian.sh

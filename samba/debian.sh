e6f5f276a50856206ad387f41754af0ad0c184de
appvise <cchong.vise@gmail.com>
2015-03-02 19:16:30 -0800

    Update debian.sh

--- samba/debian.sh
+++ samba/debian.sh
@@ -1,7 +1,15 @@
+
+echo "What is the ip address of this system?"
 read ip_address
+
+apt-get install samba-common samba -y
 
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT" >> /etc/iptables/rulez
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT" >> /etc/iptables/rulez
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 139 -j ACCEPT" >> /etc/iptables/rulez
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT" >> /etc/iptables/rulez
 iptables-restore < /etc/iptables/rulez
+
+Getsebool –a | grep samba
+Getsebool –a | grep smb
+Setsebool –P samba_domain_controller on

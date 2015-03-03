b2bdddd4855f4917c28eaf1ad2556f9b09700662
appvise <cchong.vise@gmail.com>
2015-03-02 19:13:53 -0800

    Update debian.sh

--- samba/debian.sh
+++ samba/debian.sh
@@ -1,1 +1,6 @@
 
+echo "-A RH-Firewall-1-INPUT -s 192.168.1.0/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT" >> /etc/iptables/rulez
+echo "-A RH-Firewall-1-INPUT -s 192.168.1.0/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT" >> /etc/iptables/rulez
+echo "-A RH-Firewall-1-INPUT -s 192.168.1.0/24 -m state --state NEW -p tcp --dport 139 -j ACCEPT" >> /etc/iptables/rulez
+echo "-A RH-Firewall-1-INPUT -s 192.168.1.0/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT" >> /etc/iptables/rulez
+iptables-restore < /etc/iptables/rulez

511c33cc47ce100385b28ee9788308dff2eca250
appvise <cchong.vise@gmail.com>
2015-03-02 19:34:21 -0800

    Update debian.sh

--- samba/debian.sh
+++ samba/debian.sh
@@ -1,7 +1,17 @@
+Useradd smbuser –m –G domainusers
+echo "---!!!--SET PASSWORD FOR USER--!!!---"
+echo "---!!!--SET PASSWORD FOR USER--!!!---"
+echo "---!!!--SET PASSWORD FOR USER--!!!---"
+echo "---!!!--SET PASSWORD FOR USER--!!!---"
+passwd smbuser
+
+
+
 hostname -I > ip1.txt
 ip_address=$(cut -d' ' -f1 ip1.txt)
 
 apt-get install samba-common samba -y
+
 
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT" >> /etc/iptables/rulez
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT" >> /etc/iptables/rulez
@@ -9,14 +19,15 @@ echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dpo
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT" >> /etc/iptables/rulez
 iptables-restore < /etc/iptables/rulez
 
+
 getsebool –a | grep samba
 getsebool –a | grep smb
 setsebool –P samba_domain_controller on
 
 /etc/init.d/samba start
-
+groupadd domainusers
 mkdir /var/smb
-chmod -R root:users /var/smb
+chmod -R root:domainusers /var/smb
 chown root:smbuser /var/smb
 
 echo "[smb]" >> /etc/samba/smb.conf
@@ -30,12 +41,5 @@ echo "Writeable = yes" >> /etc/samba/smb.conf
 echo "Browseable = yes" >> /etc/samba/smb.conf
 
 /etc/init.d/samba restart 
-groupadd domainusers
-Useradd smbuser –m –G domainusers
-echo "---!!!--SET PASSWORD FOR USER--!!!---"
-echo "---!!!--SET PASSWORD FOR USER--!!!---"
-echo "---!!!--SET PASSWORD FOR USER--!!!---"
-echo "---!!!--SET PASSWORD FOR USER--!!!---"
-passwd smbuser
 
 

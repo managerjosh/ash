f7f5c5b36f0b2d87b4a3ab1a91a62678293a2ff0
appvise <cchong.vise@gmail.com>
2015-03-02 21:52:49 -0800

    Update add_samba_rulez.sh

--- iptables/add_samba_rulez.sh
+++ iptables/add_samba_rulez.sh
@@ -1,10 +1,10 @@
 hostname -I > ip1.txt
 ip_address=$(cut -d' ' -f1 ip1.txt)
 
-iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT
-iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT
-iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 139 -j ACCEPT
-iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT
+iptables -A INPUT --sport $ip_address -m state --state NEW -p tcp --dport 137 -j ACCEPT
+iptables -A INPUT --sport $ip_address -m state --state NEW -p tcp --dport 138 -j ACCEPT
+iptables -A INPUT --sport $ip_address -m state --state NEW -p tcp --dport 139 -j ACCEPT
+iptables -A INPUT --sport $ip_address -m state --state NEW -p tcp --dport 445 -j ACCEPT
 
 iptables-save
 

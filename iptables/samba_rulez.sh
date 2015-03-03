bf25f184ce84fc5e2f328191e07a2b2e815d922f
appvise <cchong.vise@gmail.com>
2015-03-02 21:41:29 -0800

    Update samba_rulez.sh

--- iptables/samba_rulez.sh
+++ iptables/samba_rulez.sh
@@ -1,11 +1,6 @@
 hostname -I > ip1.txt
 ip_address=$(cut -d' ' -f1 ip1.txt)
 
-echo "-A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT" >> /etc/iptables/rulez
-echo "-A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT" >> /etc/iptables/rulez
-echo "-A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 139 -j ACCEPT" >> /etc/iptables/rulez
-echo "-A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT" >> /etc/iptables/rulez
-
 iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT
 iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT
 iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 139 -j ACCEPT

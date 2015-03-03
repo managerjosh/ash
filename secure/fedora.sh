27f8ac1ccc5a3addb18533580895b4c52820cc49
appvise <cchong.vise@gmail.com>
2015-03-02 20:23:44 -0800

    Update fedora.sh

--- secure/fedora.sh
+++ secure/fedora.sh
@@ -4,7 +4,7 @@ systemctl stop netcat
 chkconfig --level 0123456 sshd off
 
 
-echo "UPDATING SYSTEM TAKES 526 PACKAGES!!! WILL YOU UPDATE???"
+echo "UPDATING SYSTEM TAKES 526 PACKAGES!!! WILL YOU UPDATE???; USES 2GB RAM!!!???"
 read answerUpdate
 
 if (answerUpdate == yes)

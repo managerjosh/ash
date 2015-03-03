08cafc05cea03df9929b97ec6ce6fdb7d556ec30
appvise <cchong.vise@gmail.com>
2015-03-02 19:24:53 -0800

    Update debian.sh

--- samba/debian.sh
+++ samba/debian.sh
@@ -10,6 +10,33 @@ echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dpo
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT" >> /etc/iptables/rulez
 iptables-restore < /etc/iptables/rulez
 
-Getsebool –a | grep samba
-Getsebool –a | grep smb
-Setsebool –P samba_domain_controller on
+getsebool –a | grep samba
+getsebool –a | grep smb
+setsebool –P samba_domain_controller on
+
+/etc/init.d/samba start
+
+mkdir /var/smb
+chmod -R root:users /var/smb
+chown root:smbuser /var/smb
+
+echo "[smb]" >> /etc/samba/smb.conf
+echo "Comment = all domainusers" >> /etc/samba/smb.conf
+echo "Path = /var/smb" >> /etc/samba/smb.conf
+echo "Valid users = @smbuser" >> /etc/samba/smb.conf
+echo "Force group = domainusers" >> /etc/samba/smb.conf
+echo "Create mask = 0660" >> /etc/samba/smb.conf
+echo "Directory mask = 0771" >> /etc/samba/smb.conf
+echo "Writeable = yes" >> /etc/samba/smb.conf
+echo "Browseable = yes" >> /etc/samba/smb.conf
+
+/etc/init.d/samba restart 
+groupadd domainusers
+Useradd smbuser –m –G domainusers
+echo "---!!!--SET PASSWORD FOR USER--!!!---"
+echo "---!!!--SET PASSWORD FOR USER--!!!---"
+echo "---!!!--SET PASSWORD FOR USER--!!!---"
+echo "---!!!--SET PASSWORD FOR USER--!!!---"
+passwd smbuser
+
+

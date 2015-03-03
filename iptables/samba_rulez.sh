59f8c3cef162b4a79e1f39a61cab20a8b7774f4f
appvise <cchong.vise@gmail.com>
2015-03-02 21:35:32 -0800

    Update samba_rulez.sh

--- iptables/samba_rulez.sh
+++ iptables/samba_rulez.sh
@@ -1,5 +1,5 @@
-read input
-ip_address=$(read input)
+hostname -I > ip1.txt
+ip_address=$(cut -d' ' -f1 ip1.txt)
 
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT" >> /etc/iptables/rulez
 echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT" >> /etc/iptables/rulez

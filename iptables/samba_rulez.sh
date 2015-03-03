919541a66c75e089fc36251ae518f425a57abda5
appvise <cchong.vise@gmail.com>
2015-03-02 21:37:14 -0800

    Update samba_rulez.sh

--- iptables/samba_rulez.sh
+++ iptables/samba_rulez.sh
@@ -1,12 +1,12 @@
 hostname -I > ip1.txt
 ip_address=$(cut -d' ' -f1 ip1.txt)
 
-echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT" >> /etc/iptables/rulez
-echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT" >> /etc/iptables/rulez
-echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 139 -j ACCEPT" >> /etc/iptables/rulez
-echo "-A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT" >> /etc/iptables/rulez
+echo "-A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT" >> /etc/iptables/rulez
+echo "-A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT" >> /etc/iptables/rulez
+echo "-A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 139 -j ACCEPT" >> /etc/iptables/rulez
+echo "-A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT" >> /etc/iptables/rulez
 
-iptables -A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT
-iptables -A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT
-iptables -A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 139 -j ACCEPT
-iptables -A RH-Firewall-1-INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT
+iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 137 -j ACCEPT
+iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 138 -j ACCEPT
+iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 139 -j ACCEPT
+iptables -A INPUT -s $ip_address/24 -m state --state NEW -p tcp --dport 445 -j ACCEPT

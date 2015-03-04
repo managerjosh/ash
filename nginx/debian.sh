ef20c86b195fdbac8b7d0ba221cba309816febbd
appvise <cchong.vise@gmail.com>
2015-03-04 00:53:19 -0800

    Update debian.sh

--- nginx/debian.sh
+++ nginx/debian.sh
@@ -1,16 +1,17 @@
-apt-get install nginx* mysql-server
+# note need mysql-server
+apt-get install nginx* 
 service nginx start
 service nginx restart
-/etc/nginx/sites-enabled/
-/etc/nginx/sites-available/
+# /etc/nginx/sites-enabled/
+# /etc/nginx/sites-available/
 
-Reveal VPS ipaddress
+# Reveal VPS ipaddress
 ifconfig eth0 | grep inet | awk '{ print $2 }'
 
-Open up the default virtual host file with this command:
+# Open up the default virtual host file with this command:
 nano /etc/nginx/sites-available/default
-adjust listen port, root directory, index file, server name
+# adjust listen port, root directory, index file, server name
         
-Directory of live site for php
-nano /usr/share/nginx/www/info.php
-chmod o+w ./ chmod 777 wp-content -R
+# Directory of live site for php
+# nano /usr/share/nginx/www/info.php
+# chmod o+w ./ chmod 777 wp-content -R

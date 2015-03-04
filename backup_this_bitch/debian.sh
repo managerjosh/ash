47de22d95418a21777b507779f7b06937bdfb9e6
appvise <cchong.vise@gmail.com>
2015-03-04 01:11:55 -0800

    Update debian.sh

--- backup_this_bitch/debian.sh
+++ backup_this_bitch/debian.sh
@@ -1,9 +1,12 @@
 # backup mysql db
 # syntax: 
 # mysqldump -u [username] -p[password] -h [host] --all-databases > [backup].sql
+
+echo "Backing up all mysql"
 cd 
 date=$(date)
 mysqldump -u root -p -h localhost --all-databases > $date-mysqlFullBackup.sql
+echo "Backed up mysql db"
 
 # restore mysql db
 # log into sql
@@ -12,7 +15,10 @@ mysqldump -u root -p -h localhost --all-databases > $date-mysqlFullBackup.sql
 # mysql -u [username] -p[password] -h [host] [databaseName] < [filename].sql
 cd 
 tar cf etc.tar /etc/ && tar cf home.tar /home/ && tar cf var.tar /var/www /var/spool /var/mail /var/log
+echo "Backed up /etc/ /home/ /var/-www,spool,log"
 
+history | less > console_history_saved.txt
+echo "Backed up console history"
 # OR
 # tar cf system.tar /etc/ /home/ /var/ /bin/ /boot/ /proc/ /dev/ /sys/
 # Unzip: 

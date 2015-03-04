1dcc15f4e058a75af48924e06d570664204c44a2
appvise <cchong.vise@gmail.com>
2015-03-04 13:10:04 -0800

    Update freebsd.sh

--- apache/freebsd.sh
+++ apache/freebsd.sh
@@ -12,3 +12,19 @@ vi /usr/local/etc/apache24/httpd.conf
 
 service apache24 onestart
 /usr/local/sbin/apachectl start
+
+#Edit /usr/local/etc/apache24/httpd.conf file and add the following lines under /AddType
+
+echo 'AddType application/x-httpd-php .php' >> /usr/local/etc/apache24.httpd.conf
+echo 'AddType application/x-httpd-php-source .phps' /usr/local/etc/apache24.httpd.conf
+#And add the following line under LoadModule section
+
+echo 'LoadModule php5_module        libexec/apache24/libphp5.so' /usr/local/etc/apache24.httpd.conf
+
+echo "Modify the line ‘DirectoryIndex index.html’ to DirectoryIndex index.php index.html"
+sleep 2
+
+#Now restart the apache server by using the following command
+echo "Restarting Service"
+sleep 2
+/usr/local/sbin/apachectl restart

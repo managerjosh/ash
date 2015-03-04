9fdadca2ec3b47a2b4102e936bb7be523d61f1ec
appvise <cchong.vise@gmail.com>
2015-03-04 01:08:38 -0800

    Update debian.sh

--- backup_this_bitch/debian.sh
+++ backup_this_bitch/debian.sh
@@ -1,1 +1,19 @@
+# backup mysql db
+# syntax: 
+# mysqldump -u [username] -p[password] -h [host] --all-databases > [backup].sql
+cd 
+date=$(date)
+mysqldump -u root -p -h localhost --all-databases > $date-mysqlFullBackup.sql
 
+# restore mysql db
+# log into sql
+# create database with same name
+# quit
+# mysql -u [username] -p[password] -h [host] [databaseName] < [filename].sql
+cd 
+tar cf etc.tar /etc/ && tar cf home.tar /home/ && tar cf var.tar /var/www /var/spool /var/mail /var/log
+
+# OR
+# tar cf system.tar /etc/ /home/ /var/ /bin/ /boot/ /proc/ /dev/ /sys/
+# Unzip: 
+# tar –C /_directory_ -xvf _tarfile_.tar

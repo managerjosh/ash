2292428652b38f48ff37bc12f474619d4cdf2462
appvise <cchong.vise@gmail.com>
2015-03-04 13:12:57 -0800

    Update debian.sh

--- nagios/debian.sh
+++ nagios/debian.sh
@@ -1,5 +1,5 @@
 # https://www.howtoforge.com/installing-nagios-on-debian-lenny-and-monitoring-a-debian-lenny-server
 # monitor:~# apt-get install nagios3 nagios-plugins nagios-nrpe-plugin
 # server1:~# apt-get install nagios-nrpe-server nagios-plugins
-\apt-get install nagios-nrpe-server nagios3 nagios-plugins nagios-nrpe-plugin
+\apt-get install nagios-nrpe-server nagios3 nagios-plugins nagios-nrpe-plugin -y
 htpasswd -c /etc/nagios3/htpasswd.users nagiosadmin

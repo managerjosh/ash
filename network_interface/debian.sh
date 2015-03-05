47a1ce79fe9bc657d9cbe21f4a6955909ca46316
appvise <cchong.vise@gmail.com>
2015-03-04 18:52:05 -0800

    Update debian.sh

--- network_interface/debian.sh
+++ network_interface/debian.sh
@@ -3,3 +3,11 @@ cp -r ash/network_interface/interfaces /etc/network/interfaces
 
 /etc/init.d/networking restart
 ip link set eth0 up
+
+
+
+# default dhcp
+# auto lo
+# iface lo inet loopback
+# allow-hotplug eth0
+# iface eth0 inet dhcp

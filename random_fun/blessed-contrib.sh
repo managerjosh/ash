63dda8874bb1d065798ec467401d129717f91c18
appvise <cchong.vise@gmail.com>
2015-03-04 19:28:37 -0800

    Update blessed-contrib.sh

--- random_fun/blessed-contrib.sh
+++ random_fun/blessed-contrib.sh
@@ -1,13 +1,15 @@
 cd
 git clone https://github.com/yaronn/blessed-contrib.git
 mkdir /etc/blessed-contrib
+cd blessed-contrib
+npm install
 cp -r blessed-contrib/* /etc/blessed-contrib
 apt-get install curl -y
 curl -sL https://deb.nodesource.com/setup | bash -
 apt-get install nodejs
 yum install node npm -y
 pacman -S nodejs -y
-npm install
+
 echo "node /etc/blessed-contrib/examples/dashboard.js" >> ~/.bash_profile
 
 node /etc/blessed-contrib/examples/dashboard.js

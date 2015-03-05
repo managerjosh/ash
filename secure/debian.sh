034f1204791d54ee6885043478159a3809f66627
appvise <cchong.vise@gmail.com>
2015-03-04 19:23:10 -0800

    Update debian.sh

--- secure/debian.sh
+++ secure/debian.sh
@@ -40,7 +40,7 @@
 \bash ash/iptables/rulez
 \echo "deb http://ftp.us.debian.org/debian/ wheezy main" >> /etc/apt/sources.list
 \echo "deb-src http://ftp.us.debian.org/debian/ wheezy main" >> /etc/apt/sources.list
-\ echo "deb http://security.debian.org/ wheezy/updates main" >> /etc/apt/sources.list
+\echo "deb http://security.debian.org/ wheezy/updates main" >> /etc/apt/sources.list
 \echo "deb-src http://security.debian.org/ wheezy/updates main" >> /etc/apt/sources.list
 \echo "deb http://ftp.us.debian.org/debian/ wheezy-updates main" >> /etc/apt/sources.list
 \echo "deb-src http://ftp.us.debian.org/debian/ wheezy-updates main" >> /etc/apt/sources.list

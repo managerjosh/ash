804a35571984612c29f990dea11240e3f1535c22
appvise <cchong.vise@gmail.com>
2015-03-07 00:20:21 -0800

    Update debian.sh

--- java/debian.sh
+++ java/debian.sh
@@ -1,13 +1,14 @@
 # Download
 # Update
+cd
 apt-get install java-package
-wget http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jre-7u51-linux-x64.tar.gz?AuthParam=1391313765_3cdad81c5f3af7b5ef5ce047211e4c2d -O jre-7u51-linux-x64.tar.gz
-make-jpkg jre-7u51-linux-x64.tar.gz
-sudo dpkg -i oracle-j2re1.7_1.7.0+update51_amd64.deb
+
+wget http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-i586.rpm?AuthParam=1391313765_3cdad81c5f3af7b5ef5ce047211e4c2d -O java.rpm
+rpm -i java.rpm
 sudo update-alternatives --auto java
 
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.10.deb
-sudo dpkg -i elasticsearch-0.90.10.deb
+bash apt/elastic_search/apt.sh
+
 sudo mkdir /data
 sudo mkdir /data/logs
 sudo mkdir /data/data

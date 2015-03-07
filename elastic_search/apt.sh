12b09065400a191bbbcb1ef683faadf0232813e1
appvise <cchong.vise@gmail.com>
2015-03-07 01:47:17 -0800

    Update apt.sh

--- elastic_search/apt.sh
+++ elastic_search/apt.sh
@@ -5,3 +5,8 @@ echo 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main
 apt-get update && apt-get install elasticsearch
 
 update-rc.d elasticsearch defaults 95 10
+
+
+sed 53 
+sudo vi /etc/pam.d/su
+Uncomment pam_limits.so

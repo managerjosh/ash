ae87da6788400d84136bdb1684fd89b905317870
appvise <cchong.vise@gmail.com>
2015-03-07 01:03:09 -0800

    Update debian64.sh

--- java/debian64.sh
+++ java/debian64.sh
@@ -3,18 +3,20 @@
 cd
 apt-get install java-package
 
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jre-8u40-linux-x64.rpm
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jre-8u40-linux-x64.tar.gz
 
-rpm -i jre-8u40-linux-x64.rpm
+make-jpkg jre-8u40-linux-x64.tar.gz
+# sudo dpkg -i oracle-j2re1.7_1.7.0+update51_amd64.deb
 
-update-alternatives --auto java
 
-bash ash/elastic_search/apt.sh
+# update-alternatives --auto java
 
-mkdir /data
-mkdir /data/logs
-mkdir /data/data
-chown -R elasticsearch:elasticsearch /data/logs
-chown -R elasticsearch:elasticsearch /data/data
-chmod -R ug+rw /data/logs
-chmod -R ug+rw /data/data
+# bash ash/elastic_search/apt.sh
+
+# mkdir /data
+# mkdir /data/logs
+# mkdir /data/data
+# chown -R elasticsearch:elasticsearch /data/logs
+# chown -R elasticsearch:elasticsearch /data/data
+# chmod -R ug+rw /data/logs
+# chmod -R ug+rw /data/data

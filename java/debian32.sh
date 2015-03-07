dbd83a3d2a36139c814d2ab89c21934c9451028b
appvise <cchong.vise@gmail.com>
2015-03-07 00:39:45 -0800

    Create debian32.sh

---
+++ java/debian32.sh
@@ -0,0 +1,20 @@
+# Download
+# Update
+cd
+apt-get install java-package
+
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jre-8u40-linux-i586.rpm
+
+rpm -i jre-8u40-linux-i586.rpm
+
+update-alternatives --auto java
+
+bash ash/elastic_search/apt.sh
+
+mkdir /data
+mkdir /data/logs
+mkdir /data/data
+chown -R elasticsearch:elasticsearch /data/logs
+chown -R elasticsearch:elasticsearch /data/data
+chmod -R ug+rw /data/logs
+chmod -R ug+rw /data/data

0229d78f7075a34aeeb06be4addd08f2d13972b1
appvise <cchong.vise@gmail.com>
2015-03-07 01:28:30 -0800

    Update fedora64.sh

--- java/fedora64.sh
+++ java/fedora64.sh
@@ -1,11 +1,11 @@
 # Download
 # Update
 cd
-apt-get install java-package
+yum install java-1.7.0-openjdk-devel -y
 
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jre-8u40-linux-x64.rpm
+# wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jre-8u40-linux-x64.rpm
 
-rpm -i jre-8u40-linux-x64.rpm
+# rpm -i jre-8u40-linux-x64.rpm
 
 alternatives --install /usr/bin/java java /opt/jre1.8.0_25/bin/java 1
 

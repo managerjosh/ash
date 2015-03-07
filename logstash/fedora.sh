2b86de08b595017cd45c083bc524da7101df2c47
appvise <cchong.vise@gmail.com>
2015-03-07 01:55:41 -0800

    Update fedora.sh

--- logstash/fedora.sh
+++ logstash/fedora.sh
@@ -1,4 +1,8 @@
 # logstash
 
+wget https://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-1.4.2-1_2c0f5a1.noarch.rpm
+rpm -i logstash-1.4.2-1_2c0f5a1.noarch.rpm
+
 wget https://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-contrib-1.4.2-1_efd53ef.noarch.rpm
 rpm -i logstash-contrib-1.4.2-1_efd53ef.noarch.rpm
+

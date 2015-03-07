13adf8af144619ef95c05b346b7de28c5abfbcb4
appvise <cchong.vise@gmail.com>
2015-03-07 02:18:30 -0800

    Update kibana.sh

--- kibana/kibana.sh
+++ kibana/kibana.sh
@@ -1,1 +1,4 @@
 # kibanananana
+wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz
+tar xzvf kibana-4.0.1-linux-x64.tar.gz
+sudo cp -r kibana-4.0.1-linux-x64/* /var/www/

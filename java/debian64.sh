dea6d827cd2ef44ac2f8b79387994accda2f30d2
appvise <cchong.vise@gmail.com>
2015-03-07 01:20:36 -0800

    Update debian64.sh

--- java/debian64.sh
+++ java/debian64.sh
@@ -1,11 +1,14 @@
 # Download
 # Update
 cd
-apt-get install java-package
+echo "deb http://http.debian.net/debian/ wheezy main contrib" >> /etc/apt/sources.ist
+apt-get update && apt-get upgrade && apt-get install java-package
 
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jre-8u40-linux-x64.tar.gz
+rm -r jre-8u40-linux-x64.tar.*
 
-make-jpkg jre-8u40-linux-x64.tar.gz
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jre-8u40-linux-x64.tar.gz
+
+fakeroot make-jpkg jre-8u40-linux-x64.tar.gz
 # sudo dpkg -i oracle-j2re1.7_1.7.0+update51_amd64.deb
 
 

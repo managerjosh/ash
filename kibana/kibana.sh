bce8b6f47a8ddf4665260209084f20f1a68471f6
appvise <cchong.vise@gmail.com>
2015-03-07 04:28:45 -0800

    Update kibana.sh

--- kibana/kibana.sh
+++ kibana/kibana.sh
@@ -4,7 +4,10 @@ tar xzvf kibana-4.0.1-linux-x64.tar.gz
 cp -r kibana-4.0.1-linux-x64/* /var/www/
 
 yum provides \*bin/htpasswd
+
+echo "TIME TO ASSIGN PASSWORDS"
 htpasswd -c /etc/nginx/conf.d/kibana.htpasswd root
 htpasswd /etc/nginx/conf.d/kibana.htpasswd root
 htpasswd -c /etc/nginx/conf.d/kibana-write.htpasswd root
 
+echo "START KIBANA IN: /var/www/bin/kibana"

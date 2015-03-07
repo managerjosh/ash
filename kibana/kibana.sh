c34a30a345e6dc4cd0859c213e3a44e0ca8bcb77
appvise <cchong.vise@gmail.com>
2015-03-07 04:09:49 -0800

    Update kibana.sh

--- kibana/kibana.sh
+++ kibana/kibana.sh
@@ -2,3 +2,8 @@
 wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz
 tar xzvf kibana-4.0.1-linux-x64.tar.gz
 sudo cp -r kibana-4.0.1-linux-x64/* /var/www/
+
+yum provides \*bin/htpasswd
+htpasswd -c /etc/nginx/conf.d/kibana.htpasswd root
+htpasswd /etc/nginx/conf.d/kibana.htpasswd root
+htpasswd -c /etc/nginx/conf.d/kibana-write.htpasswd root

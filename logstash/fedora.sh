ffc4c4872439d0748a036f4779f121264fddc500
appvise <cchong.vise@gmail.com>
2015-03-07 02:02:47 -0800

    Update fedora.sh

--- logstash/fedora.sh
+++ logstash/fedora.sh
@@ -1,7 +1,7 @@
-# 1. install java
-# 2. install elasticsearch
-# 3. install redis
-# 4. install logstash
+# 1. install java - ash/java/fedora.sh 
+# 2. install elasticsearch - ash/java/yum.sh
+# 3. install redis - ash/redis/fedora.sh
+# 4. install logstash - ash/logstash/fedora.sh
 
 wget https://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-1.4.2-1_2c0f5a1.noarch.rpm
 rpm -i logstash-1.4.2-1_2c0f5a1.noarch.rpm

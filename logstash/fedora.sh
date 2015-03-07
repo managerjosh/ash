2511c9960e16fee72c0291800f36d11878970622
appvise <cchong.vise@gmail.com>
2015-03-07 01:52:46 -0800

    Update and rename logstash.sh to fedora.sh

---
+++ logstash/fedora.sh
@@ -0,0 +1,4 @@
+# logstash
+
+wget https://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-contrib-1.4.2-1_efd53ef.noarch.rpm
+rpm -i logstash-contrib-1.4.2-1_efd53ef.noarch.rpm
--- logstash/logstash.sh
+++
@@ -1,4 +0,0 @@
-# logstash
-
-wget https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
-tar -xzf logstash-1.4.2.tar.gz

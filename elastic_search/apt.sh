0941dfdf557ed32857385f803ae707ab1e7ba12b
appvise <cchong.vise@gmail.com>
2015-03-07 00:36:26 -0800

    Update apt.sh

--- elastic_search/apt.sh
+++ elastic_search/apt.sh
@@ -1,6 +1,6 @@
 wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
 
-echo '"deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"' >> /etc/apt/sources.list
+echo 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main' >> /etc/apt/sources.list
 
 apt-get update && apt-get install elasticsearch
 

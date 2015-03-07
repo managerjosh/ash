1f26e01444e0b932312e844de6f9d21da84c11f6
appvise <cchong.vise@gmail.com>
2015-03-07 02:15:57 -0800

    Update debian.sh

--- nginx/debian.sh
+++ nginx/debian.sh
@@ -1,7 +1,17 @@
 # note need mysql-server
-apt-get install nginx* 
+
+apt-get install nginx
+mkdir /etc/nginx/ssl
+cd /etc/nginx/ssl
+openssl genrsa -des3 -out server.key 1024
+openssl req -new -key server.key -out server.csr
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
 service nginx start
 service nginx restart
+
 # /etc/nginx/sites-enabled/
 # /etc/nginx/sites-available/
 
@@ -9,7 +19,8 @@ service nginx restart
 ifconfig eth0 | grep inet | awk '{ print $2 }'
 
 # Open up the default virtual host file with this command:
-nano /etc/nginx/sites-available/default
+
+# nano /etc/nginx/sites-available/default
 # adjust listen port, root directory, index file, server name
         
 # Directory of live site for php

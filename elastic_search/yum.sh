4b1c535b1d289d569162dcabed07cd1cafa6385a
appvise <cchong.vise@gmail.com>
2015-03-07 00:38:01 -0800

    Update yum.sh

--- elastic_search/yum.sh
+++ elastic_search/yum.sh
@@ -9,5 +9,5 @@ echo "enabled=1" >>  /etc/yum.repos.d/elasticsearch.repo
 
 yum install elasticsearch -y
 chkconfig --add elasticsearch
-sudo /bin/systemctl daemon-reload
-sudo /bin/systemctl enable elasticsearch.service
+/bin/systemctl daemon-reload
+/bin/systemctl enable elasticsearch.service

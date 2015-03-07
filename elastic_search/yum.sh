a161dd426bf3e70bb5d656086cee0b49ca1d38c0
appvise <cchong.vise@gmail.com>
2015-03-07 01:31:47 -0800

    Update yum.sh

--- elastic_search/yum.sh
+++ elastic_search/yum.sh
@@ -1,3 +1,5 @@
+bash ash/java/fedora.sh
+
 rpm --import https://packages.elasticsearch.org/GPG-KEY-elasticsearch
 touch  /etc/yum.repos.d/elasticsearch.repo
 echo "[elasticsearch-1.4]" >>  /etc/yum.repos.d/elasticsearch.repo
@@ -11,3 +13,17 @@ yum install elasticsearch -y
 chkconfig --add elasticsearch
 /bin/systemctl daemon-reload
 /bin/systemctl enable elasticsearch.service
+
+
+
+
+systemctl start elasticsearch
+chkconfig elasticsearch on
+
+mkdir /data
+mkdir /data/logs
+mkdir /data/data
+chown -R elasticsearch:elasticsearch /data/logs
+chown -R elasticsearch:elasticsearch /data/data
+chmod -R ug+rw /data/logs
+chmod -R ug+rw /data/data

ac1d65eb7a6e2b7067eb71afb17e35da28b6731b
appvise <cchong.vise@gmail.com>
2015-03-07 03:34:33 -0800

    Update fedora.sh

--- logstash/fedora.sh
+++ logstash/fedora.sh
@@ -1,9 +1,9 @@
-# 1. install java - ash/java/fedora.sh 
-# 2. install elasticsearch - ash/elastic_search/yum.sh
-# 3. install redis - ash/redis/fedora.sh
-# 4. install logstash - ash/logstash/fedora.sh
-# 5. install kibana
-# 6. install apache2/nginx
+# 1. install java - 					ash/java/fedora.sh 
+# 2. install elasticsearch - 				ash/elastic_search/yum.sh
+# 3. install redis - 					ash/redis/fedora.sh
+# 4. install logstash - 				ash/logstash/fedora.sh
+# 5. install kibana - 					ash/kibana/fedora.sh
+# 6. install apache2/nginx - 				ash/nginx/fedora.sh
 # 7. install apache2-utils 
 # 8. bind redis to localhost
 # 9. bind elasticsearch to localhost

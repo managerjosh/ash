ca6d4390f22d0e4f591c5922cbc6547312aae947
appvise <cchong.vise@gmail.com>
2015-03-07 18:20:47 -0800

    Update add_kibana_rulez.sh

--- iptables/add_kibana_rulez.sh
+++ iptables/add_kibana_rulez.sh
@@ -1,19 +1,23 @@
 hostname -I > ip1.txt
 ip_address=$(cut -d' ' -f1 ip1.txt)
 
-iptables -A INPUT -m state --state NEW -p tcp --dport 5601 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 56892 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 56900 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 56889 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 56894 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 56899 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 56891 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 56895 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 9090 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 9200 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 9300 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 22 -j REJECT
+firewall-cmd --permanent --zone=FedoraServer --add-service=http
+firewall-cmd --permanent --zone=FedoraServer --add-service=https
+firewall-cmd --permanent --zone=FedoraServer --add-port=5544/udp
+
+#iptables -A INPUT -m state --state NEW -p tcp --dport 5601 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 56892 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 56900 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 56889 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 56894 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 56899 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 56891 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 56895 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 9090 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 9200 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 9300 -j ACCEPT
+#iptables -A INPUT -m state --state NEW -p tcp --dport 22 -j REJECT
 
 iptables-save
 

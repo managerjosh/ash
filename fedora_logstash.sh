16ae7c05ad8b67e0d48e3fa86cae0410ca7cd1fa
appvise <cchong.vise@gmail.com>
2015-03-07 18:39:42 -0800

    Create fedora_logstash.sh

---
+++ fedora_logstash.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash/install-logstash-rsyslog.sh ; chmod +x install-logstash-rsyslog.sh ; ./install-logstash-rsyslog.sh
+rpm --import http://packages.elasticsearch.org/GPG-KEY-elasticsearch
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash/repository/logstash.repo
+wget https://raw.githubusercontent.com/sbagmeijer/ulyaoth/master/guides/logstash/repository/elasticsearch.repo

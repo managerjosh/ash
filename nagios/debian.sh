19967025773733cca09cac2cb66ca0caa366a43f
appvise <cchong.vise@gmail.com>
2015-03-14 09:42:49 -0700

    Update debian.sh

--- nagios/debian.sh
+++ nagios/debian.sh
@@ -3,3 +3,6 @@
 # server1:~# apt-get install nagios-nrpe-server nagios-plugins
 \apt-get install nagios-nrpe-server nagios3 nagios-plugins nagios-nrpe-plugin -y
 htpasswd -c /etc/nagios3/htpasswd.users nagiosadmin
+
+
+# http://assets.nagios.com/downloads/nagiosxi/docs/Offloading_MySQL_to_Remote%20Server.pdf

70e230a5c1e64602c924ab25c9edcef759be7832
appvise <cchong.vise@gmail.com>
2015-03-14 11:33:19 -0700

    Update ubuntu.sh

--- vlan/ubuntu.sh
+++ vlan/ubuntu.sh
@@ -1,1 +1,12 @@
 # https://wiki.ubuntu.com/vlan
+apt-get install vlan
+modprobe 8021q
+vconfig add eth1 10
+ip addr add 10.0.0.1/24 dev eth1.10
+su -c 'echo "8021q" >> /etc/modules'
+
+# auto eth1.10
+# iface eth1.10 inet static
+#     address 10.0.0.1
+#     netmask 255.255.255.0
+#     vlan-raw-device eth1

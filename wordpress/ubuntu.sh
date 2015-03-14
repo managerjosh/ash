14cb4c83b189695cd7dffb918bf01dd19a8dfc92
appvise <cchong.vise@gmail.com>
2015-03-14 05:34:22 -0700

    Update ubuntu.sh

--- wordpress/ubuntu.sh
+++ wordpress/ubuntu.sh
@@ -3,3 +3,14 @@ wget https://en-ca.wordpress.org/wordpress-3.3.1-en_CA.tar.gz
 
 tar -xzf wordpress-3.3.1-en_CA.tar.gz
 
+cp -r wordpress/* /var/www/html
+
+# edit wpconfig-sample.php file
+# add db information
+
+# On mysql server:
+# create database wordpress;
+# create uesr wpuser@localhost identified by 'password';
+# grant all privileges on wordpress.* to wpuser@localhost;
+# flush privileges
+

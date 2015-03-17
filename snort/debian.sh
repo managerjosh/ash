5d09a6b248c00c3a2e8152c4bf0779b4ed86c4b8
appvise <cchong.vise@gmail.com>
2015-03-17 11:11:14 -0700

    Update debian.sh

--- snort/debian.sh
+++ snort/debian.sh
@@ -1,6 +1,6 @@
 \apt-get install wget nano make g++ bison flex git python sed htop libpcre3-dev libdumbnet-dev zlib1g-dev libpcap-dev -y
 cd /usr/src
-wget http://www.tcpdump.org/release/libpcap-1.6.2.tar.gz  http://libdnet.googlecode.com/files/libdnet-1.12.tgz https://www.snort.org/downloads/snort/daq-2.0.4.tar.gz https://snort.org/downloads/snort/snort-2.9.7.0.tar.gz
+wget http://www.tcpdump.org/release/libpcap-1.6.2.tar.gz  http://libdnet.googlecode.com/files/libdnet-1.12.tgz https://www.snort.org/downloads/snort/daq-2.0.4.tar.gz https://snort.org/downloads/snort/snort-2.9.7.2.tar.gz
 tar -xzf libpcap-1.6.2.tar.gz && cd /usr/src/libpcap-1.6.2
 ./configure --prefix=/usr && make && make install 
 cd /usr/src/
@@ -10,13 +10,13 @@ cd /usr/src/
 tar -xzf daq-2.0.4.tar.gz && cd /usr/src/daq-2.0.4
 ./configure && make && make install
 cd /usr/src/
-tar -xzf snort-2.9.7.0.tar.gz && cd /usr/src/snort-2.9.7.0
+tar -xzf snort-2.9.7.2.tar.gz && cd /usr/src/snort-2.9.7.2
 ./configure --enable-sourcefire && make && make install
 mkdir /usr/local/etc/snort /usr/local/etc/snort/rules /var/log/snort /usr/local/lib/snort_dynamicrules
 groupadd snort && useradd -g snort snort
-cp /usr/src/snort-2.9.7.0/etc/*.conf /usr/local/etc/snort
-cp /usr/src/snort-2.9.7.0/etc/*.map /usr/local/etc/snort
-cp /usr/src/snort-2.9.7.0/etc/*.config /usr/local/etc/snort
+cp /usr/src/snort-2.9.7.2/etc/*.conf /usr/local/etc/snort
+cp /usr/src/snort-2.9.7.2/etc/*.map /usr/local/etc/snort
+cp /usr/src/snort-2.9.7.2/etc/*.config /usr/local/etc/snort
 cp /root/ash/snort/snort.conf /usr/local/etc/snort
 cp /root/ash/snort/local.rules /usr/local/etc/snort/rules
 touch /usr/local/etc/snort/rules/white_list.rules /usr/local/etc/snort/rules/black_list.rules /usr/local/etc/snort/sid-msg.map

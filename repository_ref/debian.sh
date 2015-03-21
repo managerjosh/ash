873a84e7bdeb75be7b3f9faa4dfe4184776e7be0
appvise <cchong.vise@gmail.com>
2015-03-21 14:52:08 -0700

    Update debian.sh

--- repository_ref/debian.sh
+++ repository_ref/debian.sh
@@ -1,1 +1,10 @@
 
+#http://debgen.simplylinux.ch/
+\echo "deb http://ftp.us.debian.org/debian/ wheezy main" >> /etc/apt/sources.list
+\echo "deb-src http://ftp.us.debian.org/debian/ wheezy main" >> /etc/apt/sources.list
+\echo "deb http://security.debian.org/ wheezy/updates main" >> /etc/apt/sources.list
+\echo "deb-src http://security.debian.org/ wheezy/updates main" >> /etc/apt/sources.list
+\echo "deb http://ftp.us.debian.org/debian/ wheezy-updates main" >> /etc/apt/sources.list
+\echo "deb-src http://ftp.us.debian.org/debian/ wheezy-updates main" >> /etc/apt/sources.list
+#echo "deb http://packages.dotdeb.org wheezy all" >> /etc/apt/sources.list
+#echo "deb-src http://packages.dotdeb.org wheezy all">> /etc/apt/sources.list

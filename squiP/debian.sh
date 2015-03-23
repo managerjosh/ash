6c1d522946e56a2c64b1208b911603063525fa7d
appvise <cchong.vise@gmail.com>
2015-03-23 15:27:38 -0700

    Update debian.sh

--- squiP/debian.sh
+++ squiP/debian.sh
@@ -1,4 +1,4 @@
 
 
-apt-get install build-eseential libssl-dev && apt-get install build-dep squid3
+apt-get install build-eseential libssl-dev -y && apt-get install build-dep squid3 -y
 wget http://da.gd/dibk

319f1a2527a994b828a676e3901e1eec180d745c
appvise <cchong.vise@gmail.com>
2015-03-23 20:07:09 -0700

    Update centos.sh

--- loganalyzer/centos.sh
+++ loganalyzer/centos.sh
@@ -2,3 +2,6 @@ wget http://download.adiscon.com/loganalyzer/loganalyzer-3.6.6.tar.gz
 tar -xzf loganalyzer-3.6.6.tar.gz
 mv loganalyzer-3.6.6/src/ /var/www/html/loganalyzer
 mv loganalyzer-3.6.6/contrib/* /var/www/html/loganalyzer/
+cd /var/www/html/loganalyzer/
+chmod +x configure.sh secure.sh
+./configure.sh

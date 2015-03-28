4a1ebde0f377bc301eae3ce0b0685ba83a454bf2
appvise <appvise@users.noreply.github.com>
2015-03-28 05:24:32 -0700

    Update fwsn0rt.sh

--- snort/fwsn0rt.sh
+++ snort/fwsn0rt.sh
@@ -1,3 +1,8 @@
 wget https://github.com/mrash/fwsnort/archive/master.zip -O fwsnort.zip
 unzip fwsnort.zip ; mv fwsnort-master cowbell
 perl cowbell/install.pl
+ip_address=$(ifconfig eth0 | awk -F"[: ]+" '/inet addr:/ {print $4}')
+sed -i "15s/.*/HOME_NET $ip_address;/" /etc/fwsnort.conf
+sed -i "16s/.*/EXTERNAL_NET !$$ip_address;/" /etc/fwsnort.conf
+fwsnort
+/var/lib/fwsnort/fwsnort.sh

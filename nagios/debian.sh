7dc7678e54d780caf2d9302e17b910f6a1a97147
appvise <appvise@users.noreply.github.com>
2015-03-28 02:47:05 -0700

    Update debian.sh

--- nagios/debian.sh
+++ nagios/debian.sh
@@ -2,6 +2,7 @@
 # monitor:~# apt-get install nagios3 nagios-plugins nagios-nrpe-plugin
 # server1:~# apt-get install nagios-nrpe-server nagios-plugins
 \apt-get install nagios-nrpe-server nagios3 nagios-plugins nagios-nrpe-plugin -y
+echo "changing password for nagiosadmin"
 htpasswd -c /etc/nagios3/htpasswd.users nagiosadmin
 
 

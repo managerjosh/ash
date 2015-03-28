de8b51648ae0dc0c1c94e07c76802495915851fb
appvise <appvise@users.noreply.github.com>
2015-03-28 04:29:03 -0700

    Update artillery.sh

--- artillery/artillery.sh
+++ artillery/artillery.sh
@@ -1,4 +1,5 @@
 wget https://github.com/trustedsec/artillery/archive/master.zip -O art.zip
 unzip art.zip ; mv artillery-master art
-cp ash/artillery/config /var/artillery/config
+
 python art/setup.py
+cp -r ash/artillery/config /var/artillery/config
